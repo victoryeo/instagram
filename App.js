3794470ab2b51c34ec8884376cd285e6b476dd11
Victor Yeo <victoryeo1974@gmail.com>
2020-12-26 19:55:53 +0800

    AddScreen

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import { createStackNavigator } from '@react-navigation/stack'
 import LandingScreen from './components/auth/Landing'
 import RegisterScreen from './components/auth/Register'
 import MainScreen from './components/Main'
+import AddScreen from './components/main/Add'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './redux/reducers'
@@ -80,6 +81,7 @@ export default class App extends Component {
         <NavigationContainer>
           <Stack.Navigator initialRouteName="Main">
             <Stack.Screen name="Main" component={MainScreen} options={{headerShown:false}} />
+            <Stack.Screen name="Add" component={AddScreen} />
           </Stack.Navigator>
         </NavigationContainer>
       </Provider>
