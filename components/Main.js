271cad900753222a11c1b227e89768b7558f935b
Victor Yeo <victoryeo1974@gmail.com>
2020-12-26 15:46:49 +0800

    add screen to bottom tab

--- components/Main.js
+++ components/Main.js
@@ -5,6 +5,8 @@ import { bindActionCreators } from 'redux'
 import { fetchUser } from '../redux/actions/index'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import FeedScreen from './main/Feed'
+import AddScreen from './main/Add'
+import ProfileScreen from './main/Profile'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
 const Tab = createBottomTabNavigator()
@@ -31,6 +33,16 @@ export class Main extends Component {
         <Tab.Screen name="Feed" component={FeedScreen} options={{
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="home" color={color} size={26}/>
+          )
+        }} />
+        <Tab.Screen name="Add" component={AddScreen} options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="plus-blox" color={color} size={26}/>
+          )
+        }} />
+        <Tab.Screen name="Profile" component={ProfileScreen} options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account-circle" color={color} size={26}/>
           )
         }} />
       </Tab.Navigator>
