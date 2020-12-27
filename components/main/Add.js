79bdeca566124332e38323b78175a111d48e0207
Victor Yeo <victoryeo1974@gmail.com>
2020-12-27 15:11:53 +0800

    add Savescreen

--- components/main/Add.js
+++ components/main/Add.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import { Camera } from 'expo-camera';
 import * as ImagePicker from 'expo-image-picker';
 
-export default function Add() {
+export default function Add({navigation}) {
   const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
   const [hasPermission, setHasPermission] = useState(null);
   const [camera, setCamera] = useState(null);
@@ -91,6 +91,11 @@ export default function Add() {
           pickImage()
         }}>
       </Button>
+      <Button
+        title="Save Image"
+        onPress={() =>{
+          navigation.navigate("Save", {image})
+        }} />
       {image && <Image source={{uri: image}} style={{flex:1 }}/>}
     </View>
   );
