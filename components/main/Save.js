65ae277814cda0a3090db0fef068535984693fd9
Victor Yeo <victoryeo1974@gmail.com>
2020-12-27 15:31:56 +0800

    add code to upload image to firebase firestore

--- components/main/Save.js
+++ components/main/Save.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { View, TextInput, Image, Button } from 'react-native'
 import firebase from 'firebase'
 
@@ -10,7 +10,23 @@ export default function Save(props) {
   const [caption, setCaption] = useState("")
 
   const uploadImage = async() => {
+    const childPath = `post/${firebase.auth().currentUser.uid}/${Math.random().toString(36)}`
+    const uri = props.route.params.image
+    const response = await fetch(uri)
+    const blob = await response.blob()
 
+    const task = firebase.storage().ref().child(childPath).put(blob)
+    const taskProgress = (snapshot) => { console.log(`transferred: ${snapshot.bytesTransferred}`) }
+
+    const taskCompleted = () => {
+      task.snapshot.ref.getDownloadURL().then((snapshot) => { console.log(snapshot) })
+    }
+
+    const taskError = (snapshot) => {
+     console.log(snapshot)
+    }
+
+    task.on("state_changed", taskProgress, taskError, taskCompleted)
   }
 
   return (
