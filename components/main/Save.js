52a97502ff30ebbbc2c528c8f2a225c4913fbc1c
Victor Yeo <victoryeo1974@gmail.com>
2020-12-28 20:29:09 +0800

    add upload image to firestore

--- components/main/Save.js
+++ components/main/Save.js
@@ -9,6 +9,21 @@ export default function Save(props) {
   console.log(props.route.params.image)  //log image uri
   const [caption, setCaption] = useState("")
 
+  const savePostData = (snapshot) => {
+    //upload image to firestore database
+    firebase.firestore()
+    .collection('posts').doc(firebase.auth().currentUser.uid)
+    .collection('userPosts').add({
+      snapshot,
+      caption,
+      creation: firebase.firestore.FieldValue.serverTimestamp()
+    })
+    .then(function() {
+      props.navigation.popToTop()
+    })
+
+  }
+
   const uploadImage = async() => {
     const childPath = `post/${firebase.auth().currentUser.uid}/${Math.random().toString(36)}`
     const uri = props.route.params.image
@@ -19,7 +34,10 @@ export default function Save(props) {
     const taskProgress = (snapshot) => { console.log(`transferred: ${snapshot.bytesTransferred}`) }
 
     const taskCompleted = () => {
-      task.snapshot.ref.getDownloadURL().then((snapshot) => { console.log(snapshot) })
+      task.snapshot.ref.getDownloadURL().then((snapshot) => {
+        savePostData(snapshot)
+        console.log(snapshot)
+      })
     }
 
     const taskError = (snapshot) => {
