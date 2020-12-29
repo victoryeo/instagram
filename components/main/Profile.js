76f1d36bcbae9ee0055f63732c75f69ab56c54a5
Victor Yeo <victoryeo1974@gmail.com>
2020-12-29 13:48:46 +0800

    get image from firestore and display it

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,10 +1,53 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { StyleSheet, View, Text, Image, FlatList } from 'react-native'
+import { connect } from 'react-redux'
 
-export default function Profile() {
+function Profile(props) {
+  const { currentUser, posts } = props
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={styles.containerView}>
+      <View style={styles.containerInfo}>
+        <Text>name: {currentUser.name}</Text>
+        <Text>{currentUser.email}</Text>
+      </View>
+      <View style={styles.containerGallery}>
+        <FlatList
+          numColumns={3}
+          horizontal={false}
+          data={posts}
+          renderItem={({item}) => (
+            <Image
+              style={styles.image}
+              source={{uri:item.downloadURL}}
+            />
+          )}
+        />
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  containerView: {
+    flex:1,
+    marginTop:40,
+    marginLeft: 20
+  },
+  containerInfo: {
+    margin: 20,
+  },
+  containerGallery: {
+    flex: 2,
+  },
+  image: {
+    flex: 1,
+    aspectRatio: 1/1
+  }
+})
+
+const mapStateToProps = (store) => ({
+  currentUser: store.userState.currentUser,
+  posts: store.userState.posts,
+})
+
+export default connect(mapStateToProps, null)(Profile)
