024be2b3fddd5ec138134380d771c56bc65641e6
Victor Yeo <victoryeo1974@gmail.com>
2020-12-29 13:05:08 +0800

    add fetchUserPosts

--- components/Main.js
+++ components/Main.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { fetchUser } from '../redux/actions/index'
+import { fetchUser, fetchUserPosts } from '../redux/actions/index'
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
 import FeedScreen from './main/Feed'
 import ProfileScreen from './main/Profile'
@@ -23,6 +23,7 @@ export class Main extends Component {
 
   componentDidMount() {
     this.props.fetchUser()
+    this.props.fetchUserPosts()
   }
 
   render() {
@@ -66,6 +67,7 @@ export class Main extends Component {
 const mapStateToProps = (store) => ({
   currentUser: store.userState.currentUser
 })
-const mapDispatchProps = (dispatch) => bindActionCreators({fetchUser}, dispatch)
+const mapDispatchProps = (dispatch) => bindActionCreators
+  ({fetchUser, fetchUserPosts}, dispatch)
 
 export default connect(mapStateToProps, mapDispatchProps)(Main)
