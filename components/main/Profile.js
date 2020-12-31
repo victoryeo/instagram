019b018f85be9f3fec997321bd002ea1f7b082e9
Victor Yeo <victoryeo1974@gmail.com>
2020-12-31 14:58:25 +0800

    add useEffect to show select user profile

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,16 +1,60 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, View, Text, Image, FlatList } from 'react-native'
 import { connect } from 'react-redux'
+import firebase from 'firebase'
+require('firebase/firestore')
 
 function Profile(props) {
+  const [userPosts, setUserPosts] = useState([])
+  const [user, setUser] = useState(null)
+
+  useEffect(()=> {
+    const {currentUser, posts} = props
+    console.log({currentUser, posts})
+    if (props.route.params.uid === firebase.auth().currentUser.uid) {
+      setUser(currentUser)
+      setUserPosts(posts)
+    }
+    else {
+      firebase.firestore().collection("users")
+        .doc(props.route.params.uid)
+        .get()
+        .then((snapshot) => {
+          if (snapshot.exists) {
+            setUser(snapshot.data())
+          }
+          else {
+            console.log('not exist')
+          }
+        })
+      firebase.firestore()
+        .collection("posts")
+        .doc(props.route.params.uid)
+        .collection("userPosts")
+        .orderBy("creation", "asc")
+        .get()
+        .then((snapshot) => {
+          let posts = snapshot.docs.map(doc => {
+            const data = doc.data()
+            const id = doc.id
+            return{id, ...data}
+          })
+          setUserPosts(posts)
+        })
+    }
+  })
+
+  if (user === null) {
+    return <View/>
+  }
+
   const { currentUser, posts } = props
-  console.log({currentUser})
-  console.log({posts})
+  console.log({currentUser, posts})
   return (
     <View style={styles.containerView}>
       <View style={styles.containerInfo}>
-        <Text>name: {currentUser.name}</Text>
-        <Text>{currentUser.email}</Text>
+        <Text>name: {user.name}</Text>
+        <Text>{user.email}</Text>
       </View>
       <View style={styles.containerGallery}>
         <FlatList
