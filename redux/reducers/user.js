3334a6f435ca0b12e803dc72cf1288ba7a22a583
Victor Yeo <victoryeo1974@gmail.com>
2020-12-31 16:45:31 +0800

    add reducer to fetch user following

--- redux/reducers/user.js
+++ redux/reducers/user.js
@@ -1,9 +1,11 @@
 import { USER_STATE_CHANGE } from "../constants"
 import { USER_POSTS_STATE_CHANGE } from "../constants"
+import { USER_FOLLOWING_STATE_CHANGE } from "../constants"
 
 const initialState = {
   currentUser: null,
-  posts: []
+  posts: [],
+  following: [],
 }
 
 export const user = (state = initialState, action) => {
@@ -17,6 +19,11 @@ export const user = (state = initialState, action) => {
       return {
         ...state,
         posts: action.posts
+      }
+    case 'USER_FOLLOWING_STATE_CHANGE':
+      return {
+        ...state,
+        following: action.following
       }
     default:
       return state
