f4a17007907e12a484b329adaedc8b2478ba259a
Victor Yeo <victoryeo1974@gmail.com>
2020-12-31 16:46:13 +0800

    add fetch user following function to mapDispatchProps

--- components/Main.js
+++ components/Main.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { fetchUser, fetchUserPosts } from '../redux/actions/index'
+import { fetchUser, fetchUserPosts , fetchUserFollowing} from '../redux/actions/index'
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
 import FeedScreen from './main/Feed'
 import SearchScreen from './main/Search'
@@ -26,6 +26,7 @@ export class Main extends Component {
   componentDidMount() {
     this.props.fetchUser()
     this.props.fetchUserPosts()
+    this.props.fetchUserFollowing()
   }
 
   render() {
@@ -85,6 +86,6 @@ const mapStateToProps = (store) => ({
   currentUser: store.userState.currentUser
 })
 const mapDispatchProps = (dispatch) => bindActionCreators
-  ({fetchUser, fetchUserPosts}, dispatch)
+  ({fetchUser, fetchUserPosts, fetchUserFollowing}, dispatch)
 
 export default connect(mapStateToProps, mapDispatchProps)(Main)
