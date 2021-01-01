8b9a77fecf4f72aaed74799dd6fd6b8738c66295
Victor Yeo <victoryeo1974@gmail.com>
2021-01-01 21:14:13 +0800

    add useEffect to query firestore

--- components/main/Comment.js
+++ components/main/Comment.js
@@ -1,7 +1,39 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import {View, Text, FlatList, Button, TextInput} from 'react-native'
+import firebase from 'firebase'
+require('firebase/firestore')
 
 export default function Comment(props) {
+  const [comments, setComments] = useState([])
+  const [postId, setPostId] = useState("")
+  const [text, setText] = useState("")
+
+  useEffect(()=> {
+    if (props.route.params.postId !== postId) {
+      //query to firestore
+      firebase.firestore()
+        .collection('posts')
+        .doc(props.route.params.uid)
+        .collection('userPosts')
+        .doc(props.route.params.postId)
+        .collection('comments')
+        .get()
+        .then((snapshot)=>{
+          let comments = snapshot.docs.map(doc=>{
+            const data = doc.data()
+            const id = doc.id
+            //use ... operator to return all the object in the data
+            //if data has creator and text, then all of that will be returned
+            return {id, ...data}
+          })
+          setComments(comments)
+        })
+      setPostId(props.route.params.postId)  
+    }
+  }, [props.route.params.postId])
+
   return(
-    <div></div>
+    <View>
+    </View>
   )
 }
