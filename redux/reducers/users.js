db7189671986534604948f07e5fd8578b23c48a9
Victor Yeo <victoryeo1974@gmail.com>
2021-01-01 17:32:10 +0800

    add clear data

--- redux/reducers/users.js
+++ redux/reducers/users.js
@@ -1,5 +1,6 @@
 import { USERS_DATA_STATE_CHANGE } from "../constants"
 import { USERS_POSTS_STATE_CHANGE } from "../constants"
+import { CLEAR_DATA } from "../constants"
 
 const initialState = {
   users: [],
@@ -20,6 +21,11 @@ export const users = (state = initialState, action) => {
         users: state.users.map(user=> user.uid === action.uid?{
           ...user, posts: action.posts}:
           user)
+      }
+    case 'CLEAR_DATA':
+      return {
+        users: [],
+        usersLoaded: 0,      
       }
     default:
       return state
