30404ed7b7f67103743b48f68d0594bb5c47a327
Victor Yeo <victoryeo1974@gmail.com>
2021-01-01 17:36:49 +0800

    add clear data

--- redux/reducers/user.js
+++ redux/reducers/user.js
@@ -1,6 +1,7 @@
 import { USER_STATE_CHANGE } from "../constants"
 import { USER_POSTS_STATE_CHANGE } from "../constants"
 import { USER_FOLLOWING_STATE_CHANGE } from "../constants"
+import { CLEAR_DATA } from "../constants"
 
 const initialState = {
   currentUser: null,
@@ -24,6 +25,12 @@ export const user = (state = initialState, action) => {
       return {
         ...state,
         following: action.following
+      }
+    case 'CLEAR_DATA':
+      return {
+        currentUser: null,
+        posts: [],
+        following: [],
       }
     default:
       return state
