d3069a55adcbb17df22529d72392c29f42e05c49
Victor Yeo <victoryeo1974@gmail.com>
2021-01-01 16:07:14 +0800

    rename the usersLoaded

--- redux/reducers/users.js
+++ redux/reducers/users.js
@@ -3,7 +3,7 @@ import { USERS_POSTS_STATE_CHANGE } from "../constants"
 
 const initialState = {
   users: [],
-  userLoaded: 0,
+  usersLoaded: 0,
 }
 
 export const users = (state = initialState, action) => {
@@ -16,7 +16,7 @@ export const users = (state = initialState, action) => {
     case 'USERS_POSTS_STATE_CHANGE':
       return {
         ...state,
-        usersLoaded: state.userLoaded + 1,
+        usersLoaded: state.usersLoaded + 1,
         users: state.users.map(user=> user.uid === action.uid?{
           ...user, posts: action.posts}:
           user)
