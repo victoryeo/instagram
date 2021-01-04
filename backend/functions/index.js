d1f3486107e4d26e6824cceb680173f2d08970a2
Victor Yeo <victoryeo1974@gmail.com>
2021-01-04 17:11:26 +0800

    add backend

---
+++ backend/functions/index.js
@@ -0,0 +1,32 @@
+const functions = require('firebase-functions');
+
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+//equi to firebase.firestore() in frontend
+const db = admin.firestore();
+
+// when {userId} is added to the field, onCreate is called
+export.addLike = functions.firestore.document('/posts/{creatorId}/userPosts/{postId}/likes/{userId}')
+  .onCreate((snap, context) => {
+    return db
+      .collection('posts')
+      .doc(context.params.creatorId)
+      .collection('userPosts')
+      .doc(context.params.postId)
+      .update({
+        likesCount: admin.firestore.FieldValue.increment(1)
+      })
+  })
+
+export.removeLike = functions.firestore.document('/posts/{creatorId}/userPosts/{postId}/likes/{userId}')
+  .onCreate((snap, context) => {
+    return db
+      .collection('posts')
+      .doc(context.params.creatorId)
+      .collection('userPosts')
+      .doc(context.params.postId)
+      .update({
+        likesCount: admin.firestore.FieldValue.increment(-1)
+      })
+  })
